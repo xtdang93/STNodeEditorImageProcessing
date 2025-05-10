ddb2c5213519f59e17da462f849b0ba5434873b8
xtdang93 <78628854+xtdang93@users.noreply.github.com>
2025-05-10 11:01:44 +0700

    Update LoadImageNode.cs

--- STNodeEditorImageProcessing/LoadImageNode.cs
+++ STNodeEditorImageProcessing/LoadImageNode.cs
@@ -1,1 +1,30 @@
+using System.Drawing;
+using System.Windows.Forms;
+using ST.Library.UI.NodeEditor;
+
+namespace STNodeEditorImageProcessing
+{
+    public class LoadImageNode : STNode
+    {
+        private STNodeOption m_out = new STNodeOption("Image", typeof(Bitmap), false);
+
+        public LoadImageNode()
+        {
+            this.Title = "Load Image";
+            this.OutputOptions.Add(m_out);
+            this.Size = new Size(120, 60);
+            this.Click += (s, e) =>
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "Image Files|*.jpg;*.png;*.bmp";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap bmp = new Bitmap(dlg.FileName);
+                    this[m_out] = bmp;
+                    this.Invalidate(); // Vẽ lại node
+                }
+            };
+        }
+    }
+}
 
